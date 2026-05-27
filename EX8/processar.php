3f3153f966bf01600311de7c5f5b195489ed7b6f
Caua-Roberto466 <cauaroberto211@gmail.com>
2026-05-27 19:50:42 -0300

    Exercícios de PW2

--- EX8/processar.php
+++ EX8/processar.php
@@ -3,27 +3,24 @@ $num1 = $_POST['num1'];
 $num2 = $_POST['num2'];
 $num3 = $_POST['num3'];
 
-$qdrd1 = pow($num1, 2);
-$qdrd2 = pow($num2, 2);
-$qdrd3 = pow($num3, 2);
-
-$soma = $qdrd1 + $qdrd2 + $qdrd3;
+$maior = max($num1, $num2, $num3);
+$menor = min($num1, $num2, $num3);
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Soma dos quadrados</title>
+    <title>Maior e menor número</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
-        <h3>A soma dos quadrados é:</h3>
+        <h3>Maior e menor número:</h3>
         <p class="resultado">
-            <?= number_format($soma, 1, ',', '.') ?>
+            <?= "O maior é $maior e o menor é $menor"?>
         </p>
-        <a class="back" href="ex5.html">← voltar</a>
+        <a class="back" href="ex8.html">← voltar</a>
     </div>
 </body>
 </html>
--- EX9/processar.php
+++ EX9/processar.php
@@ -1,29 +1,30 @@
 <?php
-$num1 = $_POST['num1'];
-$num2 = $_POST['num2'];
-$num3 = $_POST['num3'];
+$inicio = $_POST['inicio'];
+$fim = $_POST['fim'];
 
-$qdrd1 = pow($num1, 2);
-$qdrd2 = pow($num2, 2);
-$qdrd3 = pow($num3, 2);
+$soma = 0;
 
-$soma = $qdrd1 + $qdrd2 + $qdrd3;
+for($i = $inicio; $i <= $fim; $i++){
+    if ($i % 2 != 0){
+        $soma += $i;
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Soma dos quadrados</title>
+    <title>Soma dos impares</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
         <h3>A soma dos quadrados é:</h3>
         <p class="resultado">
-            <?= number_format($soma, 1, ',', '.') ?>
+            <?= "A soma dos impares é $soma" ?>
         </p>
-        <a class="back" href="ex5.html">← voltar</a>
+        <a class="back" href="ex9.html">← voltar</a>
     </div>
 </body>
 </html>
